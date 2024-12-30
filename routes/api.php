ecc3f5958651d22721f21355790a75827d8c669d
I WAYAN RAKA PUTRA YANA <rakaputray20@gmail.com>
2024-12-30 20:06:32 +0800

    fixing middleware showdetail, index, register, login

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,13 @@ use App\Http\Controllers\ListVillaController;
 */
 
 Route::middleware(['auth:sanctum'])->group(function(){
-    Route::post('/register', [UserController::class,'register']);
-    Route::post('/login', [UserController::class, 'login']);
     Route::post('/add-villa', [ListVillaController::class, 'store']);
+    Route::get('/show-detail-villa/{id}', [ListVillaController::class,'showDetail']);
+    Route::get('/show-all-villa',[ListVillaController::class,'index']);
 });
-Route::get('/show-detail-villa/{id}', [ListVillaController::class,'showDetail']);
-Route::get('/show-all-villa',[ListVillaController::class,'index']);
+
+Route::post('/register', [UserController::class,'register']);
+Route::post('/login', [UserController::class, 'login']);
+
 // testing
 Route::get('/kabupaten', [KabupatenController::class, 'index']);
