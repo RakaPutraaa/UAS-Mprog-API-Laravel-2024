023e5f12460ec8d1d4fc0468d677dcd833158ccf
I WAYAN RAKA PUTRA YANA <rakaputray20@gmail.com>
2024-12-30 19:42:37 +0800

    added fitur show detail villa

---
+++ app/Http/Resources/DetailListVillaResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DetailListVillaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'id_user' => $this->id_user,
+            'id_kabupaten' => $this->id_kabupaten,
+            'lokasi' => $this->lokasi,
+            'nama_villa' => $this->nama_villa,
+            'deskripsi_villa' => $this->deskripsi_villa,
+            'harga_villa' => $this->harga_villa,
+            'foto_villa' => $this->foto_villa,
+            'lokasi_villa' =>$this->lokasi_villa,
+            'date' => date_format($this->created_at, "Y/m/d H:i:s")
+        ];
+    }
+}
